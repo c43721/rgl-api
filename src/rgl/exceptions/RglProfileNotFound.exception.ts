69acf7ddbf1cdd11465af7fa070f997dd71ae1de
c43721 <rlahman.game@gmail.com>
2021-04-22 18:50:27 -0500

    fix: add data field to profile exception

--- src/rgl/exceptions/RglProfileNotFound.exception.ts
+++ src/rgl/exceptions/RglProfileNotFound.exception.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 export class RglProfileNotFound extends HttpException {
   constructor(steamId: string, message: string = 'Profile not found') {
     super(
-      HttpException.createBody({ statusCode: 200, steamId, message }),
+      HttpException.createBody({ data: { statusCode: 200, steamId, message } }),
       HttpStatus.OK,
     );
   }
